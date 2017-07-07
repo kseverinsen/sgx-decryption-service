233ad80c115010a63e7c10830cda1f90da3424ef
Kristoffer Severinsen <kristmse@ifi.uio.no>
2017-07-07 12:40:07 +0100

    working dummy server/client code

---
+++ client/dummyclient.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/sewelol/decryptionservice/decryptionservice"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewDecryptionDeviceClient(conn)
+
+	//  call DecryptRecord
+	r, err := c.DecryptRecord(context.Background(), &pb.DecryptionRequest{Ciphertext: []byte("some ciphertext"), ProofOfPresence: "{json proof..}", ProofOfExtension: "{json proof...}"})
+	if err != nil {
+		log.Fatalf("could not decrypt record: %v", err)
+	}
+	log.Printf("Plaintext: %s", r.Plaintext)
+
+	//  call GetRootTreeHash
+	rth, err := c.GetRootTreeHash(context.Background(), &pb.RootTreeHashRequest{Nonce: []byte("a long and random byte array")})
+	if err != nil {
+		log.Fatalf("could not get rth: %v", err)
+	}
+	log.Printf("RTH: %s \n Nonce: %s \n Signature: %s", rth.Rth, rth.Nonce, rth.Sig)
+
+	//  call GetPublicKey
+	pk, err := c.GetPublicKey(context.Background(), &pb.PublicKeyRequest{Nonce: []byte("a long and random byte array")})
+	if err != nil {
+		log.Fatalf("could not get quote containing the public key: %v", err)
+	}
+	log.Printf("Quote: %s \n encryption key: %s \n verification key: %s", pk.Quote, pk.RSA_EncryptionKey, pk.RSA_VerificationKey)
+}
---
+++ server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/sha256"
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/sewelol/decryptionservice/decryptionservice"
+	dev "github.com/sewelol/decryptionservice/device"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// Decryption device
+var d dev.Device
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+func (s *server) DecryptRecord(ctx context.Context, in *pb.DecryptionRequest) (*pb.Record, error) {
+	return &pb.Record{Plaintext: []byte("plaintext record")}, nil
+}
+
+func (s *server) GetRootTreeHash(ctx context.Context, in *pb.RootTreeHashRequest) (*pb.RootTreeHash, error) {
+
+	rth, signature := d.SignRootTreeHash(in.Nonce)
+	return &pb.RootTreeHash{Rth: rth, Nonce: in.Nonce, Sig: signature}, nil
+}
+
+func (s *server) GetPublicKey(ctx context.Context, in *pb.PublicKeyRequest) (*pb.Quote, error) {
+	ek, vk := d.ExportPubKey()
+	return &pb.Quote{Quote: "{QUOTE: {MRENCLAVE: \"enclave measurement\", MRSIGNER: \"author's identity measurement\", USERDATA: \"public keys\", SIGN: \"attestation signature\"}}", RSA_EncryptionKey: ek, RSA_VerificationKey: vk}, nil
+}
+
+func main() {
+	// Initialize device
+	initialRTH := sha256.Sum256([]byte(""))
+	d.Init(initialRTH[:])
+
+	// Start server
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterDecryptionDeviceServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
